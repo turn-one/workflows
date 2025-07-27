0ae052e8be85709a02304eddd57e1b51b238ed72
Nikhil Khule <nikhil.akhule1903@gmail.com>
2025-07-27 08:59:02 +0530

    Test pyspark decorator

--- dags/season_schedules.py
+++ dags/season_schedules.py
@@ -1,12 +1,23 @@
 from airflow.decorators import dag, task
+from pyspark.sql import SparkSession
 
-@dag(dag_id="seasons_schedule",)
+
+@dag(
+    dag_id="seasons_schedule",
+)
 def seasons_schedule():
     @task
     def print_season_schedule():
         print("This is the season schedule task")
         # Here you can add logic to fetch and print the season schedule
 
+    @task.pyspark(
+        conn_id="spark-kubernetes",
+    )
+    def pyspark_task(spark: SparkSession):
+        print(spark.sparkContext.getConf().getAll())
+
     print_season_schedule()
+
 
 seasons_schedule()

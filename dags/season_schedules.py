647e8d56b7c56ad72ac12fb741b15426ef544f6b
Nikhil Khule <nikhil.akhule1903@gmail.com>
2025-08-03 02:02:41 +0530

    Add `@task.pyspark` example

--- dags/season_schedules.py
+++ dags/season_schedules.py
@@ -1,23 +1,48 @@
-from airflow.decorators import dag, task
+from datetime import timedelta
 from pyspark.sql import SparkSession
+from airflow.decorators import dag, task
+from airflow.models import Variable
+import fastf1
+import pandas as pd
 
 
 @dag(
     dag_id="seasons_schedule",
 )
 def seasons_schedule():
-    @task
-    def print_season_schedule():
-        print("This is the season schedule task")
-        # Here you can add logic to fetch and print the season schedule
+    last_processed_season = int(
+        Variable.get(
+            "last_processed_season",
+            default_var=1950,
+        )
+    )
+    season = last_processed_season + 1
+
+    @task(
+        retries=3,
+        retry_delay=timedelta(minutes=2),
+        task_id="get_season_schedule",
+    )
+    def get_season_schedule(season: int):
+        with fastf1.Cache.disabled():
+            schedule = pd.DataFrame(
+                fastf1.get_event_schedule(
+                    season,
+                )
+            )
+        return schedule.to_json(
+            orient="records",
+        )
 
     @task.pyspark(
         conn_id="spark-kubernetes",
     )
-    def pyspark_task(spark: SparkSession):
-        print(spark.sparkContext.getConf().getAll())
+    def load_into_bronze_catalog(spark: SparkSession, schedule: str):
+        print(schedule)
+        spark.sql("SHOW CATALOGS").show()
 
-    print_season_schedule() >> pyspark_task()
+    schedule = get_season_schedule(season)
+    load_into_bronze_catalog(schedule)
 
 
 seasons_schedule()
